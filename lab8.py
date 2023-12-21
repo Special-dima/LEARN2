18a8dbab35f2f5ee4102f9b5cf39308235ed925a
Special-dima <phantom_love02@mail.ru>
2023-12-21 20:46:25 +0700

    Получение информации о курсе/-ах по REST

--- lab8.py
+++ lab8.py
@@ -8,3 +8,23 @@ lab8 = Blueprint('lab8',__name__)
 def main():
     return render_template('lab8/index.html')
 
+courses = [
+    {"name": "c++", "videos": 3, "price": 3000, "created_date": datetime.now()},
+    {"name": "basic", "videos": 30, "price": 450, "created_date": datetime.now()},
+    {"name": "c#", "videos": 8, "created_date": datetime.now()}
+]
+
+
+#здесь список курсов
+@lab8.route('/lab8/api/courses/', methods=['GET']) 
+def get_courses():
+    return courses
+
+
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['GET'])
+def get_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)  # возвращаем ошибку 404, если номер выходит за пределы
+    return courses[course_num]
+
+
