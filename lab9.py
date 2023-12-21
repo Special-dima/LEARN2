ca4015b713249f4b27a6d4cc1235b20627126ac5
Special-dima <phantom_love02@mail.ru>
2023-12-21 21:21:30 +0700

    обработчик ошибки 500

--- lab9.py
+++ lab9.py
@@ -12,3 +12,13 @@ def not_found(e):
     return render_template('lab9/error.html'), 404
 
 
+# Обработчик ошибки 500
+@lab9.app_errorhandler(500)
+def internal_server_error(e):
+    return render_template('lab9/error500.html'), 500
+
+# Роут, вызывающий ошибку 500
+@lab9.route('/lab9/500')
+def trigger_error():
+    # Генерируем ошибку сервера
+    1/0
