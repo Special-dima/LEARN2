ceb9c9be146db7119c895a2a36d45905667166c3
Special-dima <phantom_love02@mail.ru>
2023-12-21 20:47:34 +0700

    Удаление выбранного курса по REST

--- lab8.py
+++ lab8.py
@@ -28,3 +28,12 @@ def get_course(course_num):
     return courses[course_num]
 
 
+#удаление курса
+@lab8.route('/lab8/api/courses/<int:course_num>', methods=['DELETE'])
+def del_course(course_num):
+    if course_num < 0 or course_num >= len(courses):
+        abort(404)  
+    del courses[course_num]
+    return '', 204
+
+
